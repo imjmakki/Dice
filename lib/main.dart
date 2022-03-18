8c972f3e016daf9c5608854782b11864dfaf9fe3
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-18 14:46:31 +0300

    Flutter Practice

--- lib/main.dart
+++ lib/main.dart
@@ -22,21 +22,15 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FlatButton(
-                onPressed: () {},
-                child: Image.asset('images/dice1.png'),
-              ),
+            child: FlatButton(
+              onPressed: () {},
+              child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FlatButton(
-                onPressed: () {},
-                child: Image.asset('images/dice2.png'),
-              ),
+            child: FlatButton(
+              onPressed: () {},
+              child: Image.asset('images/dice2.png'),
             ),
           ),
         ],
